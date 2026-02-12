d1f24616df18e43c0a76b536b8d3ec53a4e86a91
Stefano Donà <90683084+stefanodona@users.noreply.github.com>
2026-02-12 22:28:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
-    "Please??? 💔",
-    "Don't do this to me...",
-    "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Sei sicura? 🤔",
+    "Cucci plis... 🥺",
+    "Se dirai di no, sarò molto triste...",
+    "Moooolto triste... 😢",
+    "Per favore??? 💔",
+    "Non farmi questoo...",
+    "Ultima possibilità! 😭",
+    "Non mi avrai mai eheheh 😜"
 ]
 
 const yesTeasePokes = [
-    "try saying no first... I bet you want to know what happens 😏",
-    "go on, hit no... just once 👀",
-    "you're missing out 😈",
-    "click no, I dare you 😏"
+    "prova a dire di no prima, scommetto che vorresti sapere cosa succede 😏",
+    "vai prova, premi no... solo una volta 👀",
+    "stai sbagliandooo 😈",
+    "ti sfido, clicca NO 😏"
 ]
 
 let yesTeasedCount = 0
